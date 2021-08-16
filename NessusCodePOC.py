c52bd7cc14113c86c2332daba94172c65d780192
harryalderton1 <57259095+harryalderton1@users.noreply.github.com>
2021-08-16 14:26:47 +0100

    pre release cosmetics

--- NessusCodePOC.py
+++ NessusCodePOC.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import ipaddress
+import sys
 from os import remove, write
 from typing import OrderedDict
 from lxml import etree
@@ -8,6 +9,7 @@ from socket import inet_aton
 import struct
 import ipaddress
 import nmap
+from colored import fg, bg, attr
 
 
 if __name__ == '__main__':
@@ -64,7 +66,11 @@ XX                                                                          XX
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 '''
 print(hacker)
+print("\n============ InfraStripper: Nessus File Parser v0.0.1  ============\n")
+
+
 options = {}
+options ['help'] = 'help'
 options['Upload File Path'] = 'Upload File Path'
 options['Nessus Target Ripper'] = 'Nessus Target Ripper'
 options['Nessus Target Hostname'] = 'Nessus Target Hostname'
@@ -73,6 +79,17 @@ options['Nessus Remove from Export'] = 'Nessus Remove from Export'
 options['Strip Services'] = 'Strip Services'
 options['Exit'] = 'Exit'
 
+def help():
+            print("Argument:                      Explanation:")
+            print("   option 1                      - help")
+            print("   option 2                      - Add nessus file")
+            print("   option 3                      - Strips all IP's from nessus file and outputs")
+            print("   option 4                      - Strips all the hostnames for each IP and outputs")
+            print("   option 5                      - For each host outputs the ports running")
+            print("   option 6                      - For each service running outputs the service running (WIP)")
+            print("   option 7                      - Exits the program")
+            print('\n')
+            askuser(options)
 
 def fileSelect():
     global file
@@ -88,9 +105,9 @@ def fileSelect():
                 inputValid2 = True
                 askuser(options)
             else:
-                print('Not a nessus file make sure the file extension is \'.nessus\'')
+                print('%sNot a nessus file make sure the file extension is \'.nessus\'%s' % (fg(1), attr(0)))
     else:
-        print('Please add a file')
+        print('%sPlease add a file%s' % (fg(1), attr(0)))
 
 
 
@@ -98,46 +115,52 @@ def fileSelect():
 def hostname():
     hostnames = []
     sorted_ips = []
-    fileopen = open(file, 'r')
-    doc = etree.parse(file)
-    lst = doc.xpath('//ReportHost')
-    for i in lst:
-        sorted_ips.append(i.xpath('@name')[0])
-    fileopen.close
-    sorted_ips.sort(reverse=True)
-    for sort in sorted_ips:
+    try:
+        file
+    except NameError:
+        print('%sPlease enter filepath%s'% (fg(1), attr(0)))
+        fileSelect()
+    else:
+        fileopen = open(file, 'r')
+        doc = etree.parse(file)
+        lst = doc.xpath('//ReportHost')
+        for i in lst:
+            sorted_ips.append(i.xpath('@name')[0])
+        fileopen.close
+        sorted_ips.sort(reverse=True)
+        for sort in sorted_ips:
 
-        try:
-            if ipaddress.ip_address(sort).version:
-                value = '0'
-                while value in sorted_ips:
-                    sorted_ips.remove(value)
-            sorted_ips = sorted(sorted_ips, key=lambda ip: struct.unpack("!L", inet_aton(ip))[0])
+            try:
+                if ipaddress.ip_address(sort).version:
+                    value = '0'
+                    while value in sorted_ips:
+                        sorted_ips.remove(value)
+                sorted_ips = sorted(sorted_ips, key=lambda ip: struct.unpack("!L", inet_aton(ip))[0])
 
-        except ValueError:
-            
-            index = sorted_ips.index(sort)
-            sorted_ips.remove(sort)
-            sorted_ips.insert(index, '0')
-            hostnames.append(sort)
-    
-    hostnames.sort()
+            except ValueError:
+                
+                index = sorted_ips.index(sort)
+                sorted_ips.remove(sort)
+                sorted_ips.insert(index, '0')
+                hostnames.append(sort)
+        
+        hostnames.sort()
 
-    for host in hostnames:
-        sorted_ips.append(host)
+        for host in hostnames:
+            sorted_ips.append(host)
 
-    for ip in sorted_ips:
-            path_ = '//ReportHost[@name="'+ip+'"]'
-            path2 = './/tag[@name="host-rdns"]'
-            
-            host = doc.xpath(path_)[0]
-            hostpath = host.xpath(path2)
+        for ip in sorted_ips:
+                path_ = '//ReportHost[@name="'+ip+'"]'
+                path2 = './/tag[@name="host-rdns"]'
+                
+                host = doc.xpath(path_)[0]
+                hostpath = host.xpath(path2)
 
-            for it in hostpath:
-                hostname = (it.xpath('text()')[0])
-                if hostname != ip:
-                    print('The hostname for host \t' + ip + ' is ' + hostname)
-    option = askuser(options)
+                for it in hostpath:
+                    hostname = (it.xpath('text()')[0])
+                    if hostname != ip:
+                        print('The hostname for host \t' + ip + '\t is \t' + hostname)
+        option = askuser(options)
 
 
 
@@ -146,37 +169,43 @@ def hostname():
 def targetripper():
     sorted2 = []
     hostnames = []
-    fileopen = open(file, 'r')
-    doc = etree.parse(file)
-    lst = doc.xpath('//ReportHost')
-    
-    for i in lst:
-        sorted2.append(i.xpath('@name')[0])
-        sorted2.sort(reverse=True)
-    fileopen.close
+    try:
+        file
+    except NameError:
+        print('%sPlease enter filepath%s' % (fg(1), attr(0)))
+        fileSelect()
+    else:
+        fileopen = open(file, 'r')
+        doc = etree.parse(file)
+        lst = doc.xpath('//ReportHost')
+        
+        for i in lst:
+            sorted2.append(i.xpath('@name')[0])
+            sorted2.sort(reverse=True)
+        fileopen.close
 
-    for sort in sorted2:
+        for sort in sorted2:
 
-        try:
-            if ipaddress.ip_address(sort).version:
-                value = '0'
-                while value in sorted2:
-                    sorted2.remove(value)
-            sorted2 = sorted(sorted2, key=lambda ip: struct.unpack("!L", inet_aton(ip))[0])
+            try:
+                if ipaddress.ip_address(sort).version:
+                    value = '0'
+                    while value in sorted2:
+                        sorted2.remove(value)
+                sorted2 = sorted(sorted2, key=lambda ip: struct.unpack("!L", inet_aton(ip))[0])
 
-        except ValueError:
-            
-            index = sorted2.index(sort)
-            sorted2.remove(sort)
-            sorted2.insert(index, '0')
-            hostnames.append(sort)
-    
-    hostnames.sort()
+            except ValueError:
+                
+                index = sorted2.index(sort)
+                sorted2.remove(sort)
+                sorted2.insert(index, '0')
+                hostnames.append(sort)
+        
+        hostnames.sort()
 
 
-    for i in sorted2:
-        print(i)
-    option = askuser(options)
+        for i in sorted2:
+            print(i)
+        option = askuser(options)
 
 
 
@@ -186,157 +215,176 @@ def target_port():
     
     sorted_ips = []
     hostnames = []
-    fileopen = open(file, 'r')
-    doc = etree.parse(file)
-    lst = doc.xpath('//ReportHost')
+    try:
+        file
+    except NameError:
+        print('%sPlease enter filepath%s'% (fg(1), attr(0)))
+        fileSelect()
+    else:
+        fileopen = open(file, 'r')
+        doc = etree.parse(file)
+        lst = doc.xpath('//ReportHost')
 
-    for i in lst:
-        sorted_ips.append(i.xpath('@name')[0])
-        sorted_ips.sort(reverse=True)
-    fileopen.close
+        for i in lst:
+            sorted_ips.append(i.xpath('@name')[0])
+            sorted_ips.sort(reverse=True)
+        fileopen.close
 
 
-    for sort in sorted_ips:
+        for sort in sorted_ips:
 
-        try:
-            if ipaddress.ip_address(sort).version:
-                value = '0'
-                while value in sorted_ips:
-                    sorted_ips.remove(value)
-            sorted_ips = sorted(sorted_ips, key=lambda ip: struct.unpack("!L", inet_aton(ip))[0])
+            try:
+                if ipaddress.ip_address(sort).version:
+                    value = '0'
+                    while value in sorted_ips:
+                        sorted_ips.remove(value)
+                sorted_ips = sorted(sorted_ips, key=lambda ip: struct.unpack("!L", inet_aton(ip))[0])
 
-        except ValueError:
+            except ValueError:
+                
+                index = sorted_ips.index(sort)
+                sorted_ips.remove(sort)
+                sorted_ips.insert(index, '0')
+                hostnames.append(sort)
+        
+        hostnames.sort()
+
+        for host in hostnames:
+            sorted_ips.append(host)
+
+        for ip in sorted_ips:
+            ports = []
             
-            index = sorted_ips.index(sort)
-            sorted_ips.remove(sort)
-            sorted_ips.insert(index, '0')
-            hostnames.append(sort)
-    
-    hostnames.sort()
+            path_ = '//ReportHost[@name="'+ip+'"]'
+            path2 = './/ReportItem'
 
-    for host in hostnames:
-        sorted_ips.append(host)
-
-    for ip in sorted_ips:
-        ports = []
+            host = doc.xpath(path_)[0]
+            hostpath = host.xpath(path2)
+            
+            for it in hostpath:
+                port = (it.xpath('@port')[0])
         
-        path_ = '//ReportHost[@name="'+ip+'"]'
-        path2 = './/ReportItem'
+                if port not in ports or port == 0:
+                    if port != '0':
+                        ports.append(port)
 
-        host = doc.xpath(path_)[0]
-        hostpath = host.xpath(path2)
-        
-        for it in hostpath:
-            port = (it.xpath('@port')[0])
-    
-            if port not in ports or port == 0:
-                if port != '0':
-                    ports.append(port)
+            ports_int = list(map(int, ports))
+            
+            ports_sorted = sorted(ports_int)
 
-        ports_int = list(map(int, ports))
-        
-        ports_sorted = sorted(ports_int)
+            if len(ports_sorted) != 0:
+                    print('For host ' + ip + ' the following ports were discovered:')
+                    for port in ports_sorted:
+                        print ("  " + str(port))
 
-        if len(ports_sorted) != 0:
-                print('For host ' + ip + ' the following ports were discovered:')
-                for port in ports_sorted:
-                    print ("  " + str(port))
-
-    option = askuser(options)
+        option = askuser(options)
 
 
 def removeExcess():
-    fileopen = open(file, 'r+')
-    doc = etree.parse(file)
-    write = open('testwrite.nessus', 'a')
-    
+    try:
+        file
+    except NameError:
+        print('%sPlease enter filepath%s'% (fg(1), attr(0)))
+        fileSelect()
+    else:
+        fileopen = open(file, 'r+')
+        doc = etree.parse(file)
+        write = open('testwrite.nessus', 'a')
+        
 
-    for severity in (doc.xpath('//ReportItem')):
-        severity2 = (severity.xpath('.//@severity'))
-        if severity2[0] == '0':
-            severity.getparent().remove(severity)
+        for severity in (doc.xpath('//ReportItem')):
+            severity2 = (severity.xpath('.//@severity'))
+            if severity2[0] == '0':
+                severity.getparent().remove(severity)
 
-    fileopen.seek(0)
-    fileopen.write(etree.tostring(doc, pretty_print=True).decode())
-    fileopen.truncate()
-    
-    print("File Saved to "  + file)
-    fileopen.close
+        fileopen.seek(0)
+        fileopen.write('<?xml version=\'1.0\' encoding=\'utf-8\'?> \n')
+        fileopen.write(etree.tostring(doc, pretty_print=True).decode())
+        fileopen.truncate()
+        
+        print("File Saved to "  + file)
+        fileopen.close
 
-    option = askuser(options)
+        option = askuser(options)
 
 
 def stripservices():
     sorted_ips = []
     hostnames = []
-    fileopen = open(file, 'r')
-    doc = etree.parse(file)
-    lst = doc.xpath('//ReportHost')
+    try:
+        file
+    except NameError:
+        print('%sPlease enter filepath%s'% (fg(1), attr(0)))
+        fileSelect()
+    else:
+        fileopen = open(file, 'r')
+        doc = etree.parse(file)
+        lst = doc.xpath('//ReportHost')
 
-    #Gets IP addresses from each Item and appends to sortedIP. Sorts IP addresses.
-    for i in lst:
-        sorted_ips.append(i.xpath('@name')[0])
-        sorted_ips.sort(reverse=True)
-    fileopen.close
+        #Gets IP addresses from each Item and appends to sortedIP. Sorts IP addresses.
+        for i in lst:
+            sorted_ips.append(i.xpath('@name')[0])
+            sorted_ips.sort(reverse=True)
+        fileopen.close
 
-    #Sorts IP addresses 
-    for sort in sorted_ips:
+        #Sorts IP addresses 
+        for sort in sorted_ips:
 
-        try:
-            #Gets IP address version
-            if ipaddress.ip_address(sort).version:
-                value = '0'
-                #Removes IP addresses without a value
-                while value in sorted_ips:
-                    sorted_ips.remove(value)
-            #Sorts IP address
-            sorted_ips = sorted(sorted_ips, key=lambda ip: struct.unpack("!L", inet_aton(ip))[0])
+            try:
+                #Gets IP address version
+                if ipaddress.ip_address(sort).version:
+                    value = '0'
+                    #Removes IP addresses without a value
+                    while value in sorted_ips:
+                        sorted_ips.remove(value)
+                #Sorts IP address
+                sorted_ips = sorted(sorted_ips, key=lambda ip: struct.unpack("!L", inet_aton(ip))[0])
 
-        except ValueError:
-            #If IP address cannot be sorted (i.e not IPV4/6) removes item, adds to new list and sorts the hostnames into order.clet
-            index = sorted_ips.index(sort)
-            sorted_ips.remove(sort)
-            sorted_ips.insert(index, '0')
-            hostnames.append(sort)
+            except ValueError:
+                #If IP address cannot be sorted (i.e not IPV4/6) removes item, adds to new list and sorts the hostnames into order.clet
+                index = sorted_ips.index(sort)
+                sorted_ips.remove(sort)
+                sorted_ips.insert(index, '0')
+                hostnames.append(sort)
+        
+        hostnames.sort()
+
+        #adds hostnames to the bottom of IP addresses
+        for host in hostnames:
+            sorted_ips.append(host)
+        #Gets Report Host from IP address in list
+        for ip in sorted_ips:
+            ports = {}
+            path_ = '//ReportHost[@name="'+ip+'"]'
+            path2 = './/ReportItem'
+
+            host = doc.xpath(path_)[0]
+            hostpath = host.xpath(path2)
+            #Gets Port from nessus file and gets the plugin.
+            for it in hostpath:
+                port = it.xpath('@port')[0]
+                getservice = (it.xpath('@pluginFamily')[0])
+                port = int(port)
+                #Port is not in ports and port doesnt equal 0 adds port to dictionary with blank secondary value
+                portlist =  port not in ports
+                if portlist == True:
+                    if port != 0:
+                        port = int(port)
+                        ports[port] = ''
+                # if service is 'service detection' get svc_name from xml 
+                if getservice == 'Service detection':
+                        ports[port] = (it.xpath('@svc_name')[0])
     
-    hostnames.sort()
 
-    #adds hostnames to the bottom of IP addresses
-    for host in hostnames:
-        sorted_ips.append(host)
-    #Gets Report Host from IP address in list
-    for ip in sorted_ips:
-        ports = {}
-        path_ = '//ReportHost[@name="'+ip+'"]'
-        path2 = './/ReportItem'
+            port_sort = sorted(ports.items(), key=lambda x: x[0])    
+            ports_sorted = OrderedDict(sorted(ports.items(), key=lambda x: x[0]))
+        
 
-        host = doc.xpath(path_)[0]
-        hostpath = host.xpath(path2)
-        #Gets Port from nessus file and gets the plugin.
-        for it in hostpath:
-            port = it.xpath('@port')[0]
-            getservice = (it.xpath('@pluginFamily')[0])
-            port = int(port)
-            #Port is not in ports and port doesnt equal 0 adds port to dictionary with blank secondary value
-            portlist =  port not in ports
-            if portlist == True:
-                if port != 0:
-                    port = int(port)
-                    ports[port] = ''
-               # if service is 'service detection' get svc_name from xml 
-            if getservice == 'Service detection':
-                    ports[port] = (it.xpath('@svc_name')[0])
-   
-
-        port_sort = sorted(ports.items(), key=lambda x: x[0])    
-        ports_sorted = OrderedDict(sorted(ports.items(), key=lambda x: x[0]))
-      
-
-        if len(port_sort) != 0:
-            for key, value in ports_sorted.items():
-                print (ip + " | " + str(value) + " | " + str(key))
-            print("\n")
-    askuser(options)
+            if len(port_sort) != 0:
+                for key, value in ports_sorted.items():
+                    print (ip + "\t|\t" + str(value) + "\t|\t" + str(key))
+                print("\n")
+        askuser(options)
 
 
 def askuser(options):
@@ -344,35 +392,38 @@ def askuser(options):
 
     index = 0
     indexValidList = []
-    print('Select a option:')
+    print('%sSelect a option:\n%s' % (fg(2), attr(0)))
     for optionName in options:
         index = index + 1
         indexValidList.extend([options[optionName]])
         print(str(index) + ') ' + optionName)
     inputValid = False
     while not inputValid:
-        inputRaw = input('option' + ': ')
+        print('\n')
+        inputRaw = input('option' + ':\t')
         inputNo = int(inputRaw) - 1
         if inputNo > -1 and inputNo < len(indexValidList):
             selected = indexValidList[inputNo]
-            print('Selected option: ' + selected)
+            print('\nSelected option: ' + selected +'\n')
             inputValid = True
         else:
-            print('Please select a valid number')
+            print('%sPlease select a valid number%s' % (fg(1), attr(0)))
 
     match inputNo:
         case 0:
-            fileSelect()
+            help()
         case 1:
-            targetripper()
+            fileSelect()
         case 2:
-            hostname()
+            targetripper()
         case 3:
-            target_port()
+            hostname()
         case 4:
-            removeExcess()
+            target_port()
         case 5:
-            stripservices()
+            removeExcess()
         case 6:
+            stripservices()
+        case 7:
             exit()
 option = askuser(options)
